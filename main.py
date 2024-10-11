ec568e32a8bb0690d6c91d803c52f65b94a24d73
chiamalite-codingal <chiamalitecodingal@gmail.com>
2024-10-11 17:35:37 +0100

    Floyd's triangle

---
+++ main.py
@@ -0,0 +1,8 @@
+n=int(input("Enter rows:"))
+number = 1
+for row in range(1,n+1): #row range depends on input
+    for col in range(1,row+1): #number val on col
+        print(number,end="  ")
+        number = number + 2 # number increases by 2
+    print("\n")
+    
